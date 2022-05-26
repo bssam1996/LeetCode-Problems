ff6f498d85f13b6ce42f2392fd994769bc2e9969
Bassam Hesham <bssam2012@gmail.com>
2022-05-26 14:29:13 +0200

    Update isValidBST.py

--- Solutions/isValidBST.py
+++ Solutions/isValidBST.py
@@ -7,7 +7,7 @@ def isValidBST(self, root: Optional[TreeNode]) -> bool:
     def valid(node, left, right):
         if not node:
             return True
-        if not (node.val < right and node.val > left):
+        if not (right > node.val > left):
             return False
 
         return (valid(node.left, left, node.val) and
