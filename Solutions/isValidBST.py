b443674155d9cb8e7f318c258238a5e21258da67
Bassam Hesham <bssam2012@gmail.com>
2022-05-26 14:23:25 +0200

    Update isValidBST.py

--- Solutions/isValidBST.py
+++ Solutions/isValidBST.py
@@ -3,14 +3,14 @@ Link: https://leetcode.com/problems/validate-binary-search-tree/submissions/
 """
 
 
-def valid(node, left, right):
-    if not node:
-        return True
-    if not (node.val < right and node.val > left):
-        return False
+def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def valid(node, left, right):
+        if not node:
+            return True
+        if not (node.val < right and node.val > left):
+            return False
 
-    return (valid(node.left, left, node.val) and
-            valid(node.right, node.val, right))
+        return (valid(node.left, left, node.val) and
+                valid(node.right, node.val, right))
 
-
-return valid(root, float("-inf"), float("inf"))
+    return valid(root, float("-inf"), float("inf"))
