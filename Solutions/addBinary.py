22df6c0eb7ae46ebf858383c1837c62a9b6bf10b
Bassam Hesham <bssam2012@gmail.com>
2022-06-04 20:52:18 +0200

    Create addBinary.py

---
+++ Solutions/addBinary.py
@@ -0,0 +1,13 @@
+"""
+Link: https://leetcode.com/problems/add-binary/
+"""
+
+
+def addBinary(a, b):
+    sum_both = 0
+    for char in range(len(a)):
+        sum_both += 2**char * int(a[-1-char])
+    print(sum_both)
+    for char in range(len(b)):
+        sum_both += 2**char * int(b[-1-char])
+    return format(sum_both,'b')
