391199e1790e9fab64e81f1139d059e34a5c9e2d
Bassam Hesham <bssam2012@gmail.com>
2022-06-12 16:14:42 +0200

    Create climbing-stairs.py

---
+++ Solutions/climbing-stairs.py
@@ -0,0 +1,13 @@
+"""
+Link:https://leetcode.com/problems/climbing-stairs/submissions/
+"""
+
+
+def climbStairs(self, n: int) -> int:
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[0] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return (dp[n])
