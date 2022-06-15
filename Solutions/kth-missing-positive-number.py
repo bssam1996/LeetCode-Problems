6d85601cbb4e468631e7f95ec6460a354463c444
Bassam Hesham <bssam2012@gmail.com>
2022-06-15 10:21:34 +0200

    Create kth-missing-positive-number.py

---
+++ Solutions/kth-missing-positive-number.py
@@ -0,0 +1,39 @@
+"""
+Link: https://leetcode.com/problems/kth-missing-positive-number/
+"""
+
+
+def findKthPositive(arr, k):
+    original_k = k
+    current = 1
+    first_element = arr[0]
+    values = [i+1 for i in range(0,first_element-1)]
+    while k > 0:
+        for element_index in range(current,len(arr)):
+            for i in range(arr[element_index - 1]+1, arr[element_index]):
+                values.append(i)
+                k -= 1
+        current = len(arr)
+        if len(values) == 0:
+            values.append(arr[-1] + 1)
+        elif arr[-1] > values[-1]:
+            values.append(arr[-1] + 1)
+        else:
+            values.append(values[-1] + 1)
+        k -= 1
+    return values[original_k-1]
+
+#
+# arr = [2,3,4,7,11]
+# k = 5
+
+# arr = [1,2,3,4]
+# k = 2
+
+# arr = [5,6,7,8,9]
+# k = 9
+
+arr = [1,13,18]
+k = 17
+
+print(findKthPositive(arr, k))
