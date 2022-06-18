659ffd624e800b2c181dca10e9157678ec857e02
Bassam Hesham <bssam2012@gmail.com>
2022-06-19 01:40:28 +0200

    Create binary-tree-level-order-traversal-ii.py

---
+++ Solutions/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,21 @@
+"""
+Link: https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+"""
+
+
+def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
+    queue = [root]
+    res = []
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        res.append(level)
+    return res[::-1]
