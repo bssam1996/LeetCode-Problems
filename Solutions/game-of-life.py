242cec6d9a4137a9bac5f181e26452680ec42c09
Bassam Hesham <bssam2012@gmail.com>
2022-06-26 19:35:58 +0200

    Update game-of-life.py

--- Solutions/game-of-life.py
+++ Solutions/game-of-life.py
@@ -2,6 +2,7 @@
 Link: https://leetcode.com/problems/game-of-life/
 """
 
+
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
         """
@@ -16,16 +17,16 @@ class Solution:
                 total_sum = 0
                 # Above Rows
                 if row > 0:
-                    total_sum += sum(alternative_board[ro w -1][max(colum n -1 ,0):min(colum n +2 ,num_columns)])
+                    total_sum += sum(alternative_board[row - 1][max(column - 1, 0):min(column + 2, num_columns)])
                 # Below Rows
-                if row < num_row s -1:
-                    total_sum += sum(alternative_board[ro w +1][max(colum n -1 ,0):min(colum n +2 ,num_columns)])
+                if row < num_rows - 1:
+                    total_sum += sum(alternative_board[row + 1][max(column - 1, 0):min(column + 2, num_columns)])
                 # Left Column
                 if column > 0:
-                    total_sum += alternative_board[row][colum n -1]
+                    total_sum += alternative_board[row][column - 1]
                 # Left Column
                 if column < num_columns - 1:
-                    total_sum += alternative_board[row][colum n +1]
+                    total_sum += alternative_board[row][column + 1]
                 if total_sum > 3 or total_sum < 2:
                     board[row][column] = 0
                 elif total_sum == 3:
